361f7416b6d01751f490bed2d9d07859122b57c0
zabronax <zabronax@gmail.com>
2025-11-20 11:29:45 +0100

    setup post message handler

--- Program.cs
+++ Program.cs
@@ -2,5 +2,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.MapPost("/message", (MessageCreateInfo createInfo) =>
+{
+  Console.WriteLine($"New Message from: {createInfo.Alias}. Content: {createInfo.Content}");
+  return "Message accepted";
+});
+
 // Start server
 app.Run();
+
+class MessageCreateInfo
+{
+  public required string Alias { get; set; }
+  public required string Content { get; set; }
+}
