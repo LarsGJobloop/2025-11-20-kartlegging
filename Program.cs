5bb426961e123ae131a9b84928a9497ba26da405
zabronax <zabronax@gmail.com>
2025-11-20 11:40:28 +0100

    setup get messages handler and persistence (in memory)

--- Program.cs
+++ Program.cs
@@ -2,10 +2,21 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var messages = new List<Message>();
+
 app.MapPost("/message", (MessageCreateInfo createInfo) =>
 {
   Console.WriteLine($"New Message from: {createInfo.Alias}. Content: {createInfo.Content}");
+
+  var newMessage = new Message(createInfo);
+  messages.Add(newMessage);
+
   return "Message accepted";
+});
+
+app.MapGet("/message", () =>
+{
+  return messages;
 });
 
 // Start server
@@ -15,4 +26,18 @@ class MessageCreateInfo
 {
   public required string Alias { get; set; }
   public required string Content { get; set; }
+}
+
+class Message
+{
+  public Guid Id { get; set; }
+  public string Alias { get; set; }
+  public string Content { get; set; }
+
+  public Message(MessageCreateInfo createInfo)
+  {
+    Id = Guid.NewGuid(); // Generate a new Globaly Unique Identifier
+    Alias = createInfo.Alias;
+    Content = createInfo.Content;
+  }
 }
